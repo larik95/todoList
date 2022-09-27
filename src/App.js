a82005d41a41c50ae554cc9cf1927e4aac2d01e1
Larissa <larissaferreira_@live.com>
2022-09-27 19:03:26 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,146 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
+import { data } from "./data";
 function App() {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState(data);
+  const [filter, setFilter] = useState("all");
+
+// INPUT text
+  const handleSubmit = (e) => {
+    e.preventDefault();
+   setTodos([...todos, {
+    id: Math.floor(Math.random() * 10000),
+    text: input,
+    isCompleted: false,
+   }]);
+   setInput(' ');
+  }
+//CHANGE THEME 
+const toggleTheme = () => {
+  const app = document.querySelector(".App");
+  app.classList.toggle("dark")
+}
+//REMOVE TODO
+const handleRemoveTodo = (id) => {
+  const newTodos = todos.filter((todo) => todo.id !== id);
+
+  setTodos(newTodos);
+};
+//COMPLETED TODO
+const handleCompleted = (id) => {
+  const newTodos = todos.map((todo) => {
+    if (todo.id === id) {
+      todo = { ...todo, isCompleted: !todo.isCompleted };
+    }
+    return todo;
+  });
+
+  setTodos(newTodos);
+};
+//CLEAR COMPLETED
+const clearCompleted = () => {
+  const newTodos = todos.filter((todo) => todo.isCompleted === false);
+
+  setTodos(newTodos);
+};
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="header">
+      <div className="container">
+        <div className="title">
+          <h1>Todo</h1>
+          <button className="btn" onClick={toggleTheme}></button>
+        </div>
+        <div className="todo-form">
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name="text"
+              id="todo-form-input"
+              placeholder="Create a new todo"
+              onChange={(e) => setInput(e.target.value)}
+              value={input}
+            />
+          </form>
+        </div>
+      </div>
+    </header>
+    <main className="container app-container">
+      <div className="todo-list">
+        {todos
+          .filter((todo) => {
+            if (filter === "all") return todo;
+            if (filter === "active") {
+              if (!todo.isCompleted) {
+                return todo;
+              }
+            }
+            if (filter === "completed") {
+              if (todo.isCompleted) {
+                return todo;
+              }
+            }
+          })
+          .map((todo, index) => {
+            return (
+              <div className="todo-container">
+                <div className="todo" key={index}>
+                  <input
+                    id={todo.id}
+                    type="checkbox"
+                    className="custom-checkbox"
+                    checked={todo.isCompleted ? true : false}
+                    onChange={() => handleCompleted(todo.id)}
+                    value={todo.isCompleted}
+                  />
+                  <label
+                    className="text-todo"
+                    key={todo.id}
+                  >
+                    {todo.text}
+                  </label>
+                </div>
+                <button
+                  className="btn"
+                  onClick={() => handleRemoveTodo(todo.id)}
+                ></button>
+              </div>
+            );
+          })}
+      </div>
+      <div className="todo-list-footer">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {todos.filter((todo) => todo.isCompleted === false).length}
+          &nbsp;items left{" "}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button className="btn" onClick={clearCompleted}>
+          Clear Completed
+        </button>
+      </div>
+    </main>
+    <footer>
+      <div className="filters container">
+        <button
+          className={`btn ${filter === "all" && "active"}`}
+          onClick={() => setFilter("all")}
         >
-          Learn React
-        </a>
-      </header>
+          All
+        </button>
+        <button
+          className={`btn ${filter === "active" && "active"}`}
+          onClick={() => setFilter("active")}
+        >
+          Active
+        </button>
+        <button
+          className={`btn ${filter === "completed" && "active"}`}
+          onClick={() => setFilter("completed")}
+        >
+          Completed
+        </button>
+      </div>
+    </footer>
     </div>
   );
 }
---
+++ style-guide.md
@@ -0,0 +1,46 @@
+# Front-end Style Guide
+
+## Layout
+
+The designs were created to the following widths:
+
+- Mobile: 375px
+- Desktop: 1440px
+
+## Colors
+
+### Primary
+
+- Bright Blue: hsl(220, 98%, 61%)
+- Check Background: linear-gradient #57ddff to #c058f3
+
+### Neutral
+
+### Light Theme
+
+- Very Light Gray:#fafafa
+- Very Light Grayish Blue: #e4e5f1
+- Light Grayish Blue: #e4e5f1
+- Dark Grayish Blue: #9394a5
+- Very Dark Grayish Blue: hsl(235, 19%, 35%)
+
+### Dark Theme
+
+- Very Dark Blue: hsl(235, 21%, 11%)
+- Very Dark Desaturated Blue: hsl(235, 24%, 19%)
+- Light Grayish Blue: hsl(234, 39%, 85%)
+- Light Grayish Blue (hover): hsl(236, 33%, 92%)
+- Dark Grayish Blue: hsl(234, 11%, 52%)
+- Very Dark Grayish Blue: hsl(233, 14%, 35%)
+- Very Dark Grayish Blue: hsl(237, 14%, 26%)
+
+## Typography
+
+### Body Copy
+
+- Font size: 18px
+
+### Font
+
+- Family: [Josefin Sans](https://fonts.google.com/specimen/Josefin+Sans)
+- Weights: 400, 700
